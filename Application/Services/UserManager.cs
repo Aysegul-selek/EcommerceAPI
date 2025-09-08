208d0253be42a778759394445602571208411714
emre almamis <ealmamis56@gmail.com>
2025-09-08 15:57:53 +0300

    userManager gereksiz kodlar kaldırıldı

--- Application/Services/UserManager.cs
+++ Application/Services/UserManager.cs
@@ -4,42 +4,21 @@ using Application.Exceptions;
 using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
 using Domain.Entities;
-using Microsoft.AspNetCore.Http;
-using System.Linq;
-using System.Security.Claims;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System;
+
 
 namespace Application.Services
 {
     public class UserManager : IUserService
     {
         private readonly IUserRepository _userRepository;
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public UserManager(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
+        public UserManager(IUserRepository userRepository)
         {
             _userRepository = userRepository;
-            _httpContextAccessor = httpContextAccessor;
-        }
-
-        private string? CurrentUserEmail => _httpContextAccessor.HttpContext?.User?.Claims
-            .FirstOrDefault(c =>
-                c.Type == ClaimTypes.Email ||
-                c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
-            )?.Value;
-
-        private void EnsureUserAuthorized()
-        {
-            if (string.IsNullOrEmpty(CurrentUserEmail))
-                throw new UnauthorizedException("Kullanıcı yetkili değil veya token geçersiz.");
         }
 
         public async Task<ApiResponseDto<object>> AddUser(CreateUserDto user)
         {
-            EnsureUserAuthorized();
-
             var existing = await _userRepository.GetByEmailAsync(user.Email);
             if (existing != null)
                 throw new ConflictException("Bu email ile zaten kayıtlı kullanıcı var.");
@@ -64,8 +43,6 @@ namespace Application.Services
 
         public async Task<ApiResponseDto<object>> UpdateUser(UpdateUserDto user, int id)
         {
-            EnsureUserAuthorized();
-
             var userDb = await _userRepository.FindByIdAsync(id);
             if (userDb is null)
                 throw new NotFoundException("Kullanıcı bulunamadı");
@@ -86,8 +63,6 @@ namespace Application.Services
 
         public async Task<ApiResponseDto<User>> GetUserById(int userId)
         {
-            EnsureUserAuthorized();
-
             var user = await _userRepository.FindByIdAsync(userId);
             if (user is null)
                 throw new NotFoundException("Kullanıcı bulunamadı");
@@ -102,8 +77,6 @@ namespace Application.Services
 
         public async Task<ApiResponseDto<IEnumerable<User>>> GetAllUsers()
         {
-            EnsureUserAuthorized();
-
             var users = await _userRepository.GetAllAsync();
             return new ApiResponseDto<IEnumerable<User>>
             {
@@ -115,8 +88,6 @@ namespace Application.Services
 
         public async Task<ApiResponseDto<object>> DeleteUser(long id)
         {
-            EnsureUserAuthorized();
-
             var userDb = await _userRepository.FindByIdAsync(id);
             if (userDb is null)
                 throw new NotFoundException("Kullanıcı bulunamadı");
