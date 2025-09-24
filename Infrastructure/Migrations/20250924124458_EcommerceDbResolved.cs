7bc78135dd5e6be845d8aa3e06b18a1ecba9ae7d
Aysegul-selek <aysegul.selek03@gmail.com>
2025-09-24 15:47:55 +0300

    Update-database

---
+++ Infrastructure/Migrations/20250924124458_EcommerceDbResolved.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EcommerceDbResolved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_IdempotencyRequests_Key",
+                table: "IdempotencyRequests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "IdempotencyRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "IdempotencyRequests",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdempotencyRequests_Key",
+                table: "IdempotencyRequests",
+                column: "Key",
+                unique: true);
+        }
+    }
+}
