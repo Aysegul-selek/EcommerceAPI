59ff1cad7dd0f37afb5776b0f9af93461bd0fa7c
Aysegul-selek <aysegul.selek03@gmail.com>
2025-10-03 17:51:55 +0300

    fixed type

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -152,8 +152,8 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    //c.RoutePrefix = string.Empty; // Ana sayfada Swagger açmak için
-    c.RoutePrefix = "swagger";
+    c.RoutePrefix = string.Empty; // Ana sayfada Swagger açmak için
+    //c.RoutePrefix = "swagger";
 });
 
 app.UseIpRateLimiting();
