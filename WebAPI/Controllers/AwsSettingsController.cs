42011098130f6283b8a34df677a6383d36b49808
emre almamis <ealmamis56@gmail.com>
2025-10-04 16:11:25 +0300

    aws contreller commente alındı

--- WebAPI/Controllers/AwsSettingsController.cs
+++ WebAPI/Controllers/AwsSettingsController.cs
@@ -1,39 +1,39 @@
-﻿using Application.Dtos.aws;
-using Application.Interfaces.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿//using Application.Dtos.aws;
+//using Application.Interfaces.Services;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.Controllers
-{
-    [Authorize(Roles = "Admin")]
-    [Route("api/v1/aws")]
-    [ApiController]
-    public class AwsSettingsController : ControllerBase
-    {
-        private readonly IAwsSettingsService _awsSettingsService;
+//namespace WebAPI.Controllers
+//{
+//    [Authorize(Roles = "Admin")]
+//    [Route("api/v1/aws")]
+//    [ApiController]
+//    public class AwsSettingsController : ControllerBase
+//    {
+//        private readonly IAwsSettingsService _awsSettingsService;
 
-        public AwsSettingsController(IAwsSettingsService awsSettingsService)
-        {
-            _awsSettingsService = awsSettingsService;
-        }
+//        public AwsSettingsController(IAwsSettingsService awsSettingsService)
+//        {
+//            _awsSettingsService = awsSettingsService;
+//        }
 
 
-        [HttpGet("GetSettings")]
-        public async Task<IActionResult> GetSettings()
-        {
-            var response = await _awsSettingsService.GetAsync();
-            if (!response.Success) return NotFound(response);
-            return Ok(response);
-        }
+//        [HttpGet("GetSettings")]
+//        public async Task<IActionResult> GetSettings()
+//        {
+//            var response = await _awsSettingsService.GetAsync();
+//            if (!response.Success) return NotFound(response);
+//            return Ok(response);
+//        }
 
-        [Authorize(Roles = "Admin")]
-        [HttpPost("AddSettings")]
-        public async Task<IActionResult> SaveOrUpdateSettings([FromBody] AwsSettingsDto dto)
-        {
-            var response = await _awsSettingsService.SaveOrUpdateAsync(dto);
-            if (!response.Success) return BadRequest(response);
-            return Ok(response);
-        }
+//        [Authorize(Roles = "Admin")]
+//        [HttpPost("AddSettings")]
+//        public async Task<IActionResult> SaveOrUpdateSettings([FromBody] AwsSettingsDto dto)
+//        {
+//            var response = await _awsSettingsService.SaveOrUpdateAsync(dto);
+//            if (!response.Success) return BadRequest(response);
+//            return Ok(response);
+//        }
 
-    }
-}
+//    }
+//}
